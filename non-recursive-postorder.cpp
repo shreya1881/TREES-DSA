bc933fff741b9b7f36e8658f0bc98d04a30c5ce5
shreya1881 <2704shreya@gmail.com>
2022-10-04 22:11:07 +0530

    non-recursive

---
+++ non-recursive-postorder.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+        
+        node(int d){
+            this->data=d;
+            this->left=NULL;
+            this->right=NULL;
+        }
+};
+
+node *buildTree(node* root){
+    cout<<"Enter the data: ";
+    int data;
+    cin>>data;
+    root = new node(data);
+    
+    if(data == -1)
+    return NULL;
+    
+    cout<<"Enter the value to the left of "<<data<<": ";
+    root->left=buildTree(root->left);
+    cout<<"Enter the value to the right of "<<data<<": ";
+    root->right=buildTree(root->right);
+    return root;
+}
+
+void levelOrderTraversal(node* root){
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty())
+    {
+        node* temp=q.front();
+        q.pop();
+        if(temp == NULL)
+        {
+            cout<<endl;
+            if(!q.empty())
+            q.push(NULL);
+        }
+            else{
+                cout<<temp->data<<" ";
+                if(temp->left!=NULL)
+                q.push(temp->left);
+                if(temp->right!=NULL)
+                q.push(temp->right);
+            }
+    }
+}
+
+void postorder(node* root){
+  //base condition
+  if(root==NULL)
+  return;
+  stack<node*>s1,s2;
+  node* temp=root;
+  s1.push(temp);
+  while(!s1.empty()){
+    temp=s1.top();
+    s1.pop();
+    s2.push(temp);
+    if(temp->left!=NULL) s1.push(temp->left);
+     if(temp->right!=NULL) s1.push(temp->right);
+  }
+  while(!s2.empty()){
+    cout<<s2.top()->data<<" ";
+    s2.pop();
+  }
+}
+
+int main()
+{
+    node* root=NULL;
+    root = buildTree(root);
+    levelOrderTraversal(root);
+    postorder(root);
+    return 0;
+}
