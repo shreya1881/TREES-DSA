330b1f6bacc22d12e9d19d3603080ef777c7d49f
shreya1881 <2704shreya@gmail.com>
2022-10-04 21:24:19 +0530

    traeversals

---
+++ inorder.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+        
+        node(int d){
+            this->data=d;
+            this->left=NULL;
+            this->right=NULL;
+        }
+};
+
+node *buildTree(node* root){
+    cout<<"Enter the data: ";
+    int data;
+    cin>>data;
+    root = new node(data);
+    
+    if(data == -1)
+    return NULL;
+    
+    cout<<"Enter the value to the left of "<<data<<": ";
+    root->left=buildTree(root->left);
+    cout<<"Enter the value to the right of "<<data<<": ";
+    root->right=buildTree(root->right);
+    return root;
+}
+
+void levelOrderTraversal(node* root){
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty())
+    {
+        node* temp=q.front();
+        q.pop();
+        if(temp == NULL)
+        {
+            cout<<endl;
+            if(!q.empty())
+            q.push(NULL);
+        }
+            else{
+                cout<<temp->data<<" ";
+                if(temp->left!=NULL)
+                q.push(temp->left);
+                if(temp->right!=NULL)
+                q.push(temp->right);
+            }
+    }
+}
+
+void inorder(node* root){
+  //base condition
+  if(root==NULL)
+  return;
+  inorder(root->left);
+  cout<<root->data<<" ";
+  inorder(root->right);
+}
+
+int main()
+{
+    node* root=NULL;
+    root = buildTree(root);
+    levelOrderTraversal(root);
+    inorder(root);
+    return 0;
+}
